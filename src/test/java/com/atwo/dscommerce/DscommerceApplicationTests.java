c9f0a70679e8ac84a7982bd1d411b12c52bec8be
Antonio G. <antoniomigom@gmail.com>
2025-12-17 21:25:58 -0300

    setting h2 as test environment db

---
+++ src/test/java/com/atwo/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atwo.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
