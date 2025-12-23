5ab9b10954ed9b926fdcdc7c177bb4830634f416
Antonio G. <antoniomigom@gmail.com>
2025-12-23 17:52:58 -0300

    adding controller and hashcode/equals to entities

---
+++ src/main/java/com/atwo/dscommerce/controllers/ProductController.java
@@ -0,0 +1,16 @@
+package com.atwo.dscommerce.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+    @GetMapping
+    public String teste() {
+        return "olá mundo";
+    }
+}
